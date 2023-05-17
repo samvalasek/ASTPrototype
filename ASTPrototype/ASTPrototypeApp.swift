d62d07ec54c434cdb5c3a3e2145ac64435e69446
Samuel Valasek <samuelvalasekcontact@gmail.com>
2023-05-17 09:18:53 +0100

    Initial Commit

---
+++ ASTPrototype/ASTPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASTPrototypeApp.swift
+//  ASTPrototype
+//
+//  Created by Sam Valasek on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ASTPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
